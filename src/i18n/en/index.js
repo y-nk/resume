eaaff3dd179a2bb354df830205b646f718501744
Julien Barbay <julien.barbay@gmail.com>
2022-11-25 15:14:20 +0700

    adding taskworld

--- src/i18n/en/index.js
+++ src/i18n/en/index.js
@@ -47,30 +47,38 @@ export default {
 
   situation: '{0}yo, currently in {1}',
 
-  contact: [{
-    href: 'tel:+660957321060',
-    text: '+66 957 321 060',
-  }, {
-    href: 'mailto:julien.barbay+resume@gmail.com',
-    text: 'julien.barbay@gmail.com',
-  }, {
-    href: 'https://www.linkedin.com/in/barbayjulien/',
-    text: 'in.com/barbay.julien',
-  }],
+  contact: [
+    {
+      href: 'tel:+660957321060',
+      text: '+66 957 321 060',
+    },
+    {
+      href: 'mailto:julien.barbay+resume@gmail.com',
+      text: 'julien.barbay@gmail.com',
+    },
+    {
+      href: 'https://www.linkedin.com/in/barbayjulien/',
+      text: 'in.com/barbay.julien',
+    },
+  ],
 
-  languages: [{
-    text: 'English',
-    level: 'Bilingual',
-    locale: 'en',
-  }, {
-    text: 'French',
-    level: 'Native',
-    locale: 'fr',
-  }, {
-    text: 'Japanese',
-    level: 'JLPT-N3',
-    locale: 'jp',
-  }],
+  languages: [
+    {
+      text: 'English',
+      level: 'Bilingual',
+      locale: 'en',
+    },
+    {
+      text: 'French',
+      level: 'Native',
+      locale: 'fr',
+    },
+    {
+      text: 'Japanese',
+      level: 'JLPT-N3',
+      locale: 'jp',
+    },
+  ],
 
   about: `I'm a senior frontend developer, a team leader and a tech advisor in my own field.
 
@@ -80,109 +88,130 @@ At work, _my first motivation is dealing with the impossible_. Technical challen
 
 **I'm always looking for the unknown thing that will keep me awake late at night, for the need to master it. That's what taught me so much so far, that's what will keep me up to date in 10 years.**`,
 
-  experiences: [{
-    title: 'Senior Web Engineer',
-    company: 'Amity',
-    website: 'https://amity.co/',
-    from: new Date('2020-06-01'),
-    to: null,
-    location: 'Bangkok, Thailand',
+  experiences: [
+    {
+      title: 'Solution Architect',
+      company: 'Taskworld',
+      website: 'https://taskworld.com/',
+      from: new Date('2022-01-01'),
+      to: null,
+      location: 'Bangkok, Thailand',
 
-    summary: `I'm in charge of the web sdk of "Amity Social Cloud" product, as well as a library of React components which pair with it and 3 websites to help managing our product line. For this I'm actively coding and also overseeing a growing team of remote web engineers. On the side I'm also proactively helping im designing new features (from user requirement to software architecture), and also improving our server side and platform architectures to keep up with the scale relative to our growth.`
-  }, {
-    title: 'Senior Frontend Engineer',
-    company: 'LINE',
-    website: 'https://linecorp.com/',
-    from: new Date('2018-03-01'),
-    to: new Date('2020-04-15'),
-    location: 'Kyoto, Japan',
+      summary: `I've joined the team of principal engineers which daily job is to support the feature team and provide insights on technical solutions whenever needed. My specialty in this team is about software and cloud architecturing, best practices and techniques to support the growth of our platform at scale, as well as technical documentation through the initiative of modeling our platform with the C4 model.`,
+    },
+    {
+      title: 'Senior Web Engineer',
+      company: 'Amity',
+      website: 'https://amity.co/',
+      from: new Date('2020-06-01'),
+      to: new Date('2022-01-01'),
+      location: 'Bangkok, Thailand',
 
-    summary: `At LINE, I've started, defined and implemented a production ready Design System on my own and nurtured it until it finally took off company wide ; it is now spread all around and the whole group is taking steps towards it. While doing it, I've been part of the developer's platform refactoring project, involving in taking over legacy code, analyzing it and building a new version from scratch in Vue.js.`
-  }, {
-    title: 'Tech Lead, UX Dpt',
-    company: 'Technicolor',
-    website: 'https://technicolor.com/',
-    from: new Date('2015-03-01'),
-    to: new Date('2018-02-28'),
-    location: 'Paris, France',
+      summary: `I'm in charge of the web sdk of "Amity Social Cloud" product, as well as a library of React components which pair with it and 3 websites to help managing our product line. For this I'm actively coding and also overseeing a growing team of remote web engineers. On the side I'm also proactively helping im designing new features (from user requirement to software architecture), and also improving our server side and platform architectures to keep up with the scale relative to our growth.`,
+    },
+    {
+      title: 'Senior Frontend Engineer',
+      company: 'LINE',
+      website: 'https://linecorp.com/',
+      from: new Date('2018-03-01'),
+      to: new Date('2020-04-15'),
+      location: 'Kyoto, Japan',
 
-    summary: `I'm the technical advisor for the VP of UX in Technicolor. We design products and solutions for the Internet of Thing. My daily goals are to provide prototypes, test UIs, advise designers and challenge their ideas ; find, audit and benchmark solutions, softwares and companies that could bring our concepts to production.`,
+      summary: `At LINE, I've started, defined and implemented a production ready Design System on my own and nurtured it until it finally took off company wide ; it is now spread all around and the whole group is taking steps towards it. While doing it, I've been part of the developer's platform refactoring project, involving in taking over legacy code, analyzing it and building a new version from scratch in Vue.js.`,
+    },
+    {
+      title: 'Tech Lead, UX Dpt',
+      company: 'Technicolor',
+      website: 'https://technicolor.com/',
+      from: new Date('2015-03-01'),
+      to: new Date('2018-02-28'),
+      location: 'Paris, France',
 
-    highlight: `Our multi-device, web-based content-streaming app prototype with multi-screen, AI powered personnalized recommendations. I had to code it from scratch, using React as a base, and made it to the CES in 2016.`,
-  }, {
-    title: 'Frontend Lead Developer',
-    company: 'KRDS',
-    website: 'https://krds.com/',
-    from: new Date('2013-06-01'),
-    to: new Date('2015-01-01'),
-    location: 'Chennai, India',
+      summary: `I'm the technical advisor for the VP of UX in Technicolor. We design products and solutions for the Internet of Thing. My daily goals are to provide prototypes, test UIs, advise designers and challenge their ideas ; find, audit and benchmark solutions, softwares and companies that could bring our concepts to production.`,
 
-    summary: `I'm in charge of developing and managing Flash and JS parts of web applications related to our core target (Facebook apps). I'm also the frontend consultant of the team ; I also write technical documentations and business recommandations.`,
+      highlight: `Our multi-device, web-based content-streaming app prototype with multi-screen, AI powered personnalized recommendations. I had to code it from scratch, using React as a base, and made it to the CES in 2016.`,
+    },
+    {
+      title: 'Frontend Lead Developer',
+      company: 'KRDS',
+      website: 'https://krds.com/',
+      from: new Date('2013-06-01'),
+      to: new Date('2015-01-01'),
+      location: 'Chennai, India',
 
-    highlight: `Induction plan for javascript developers made for indian people : I had to sum up my own experience and organize it in lessons to teach people despite cultural and skill differences.`,
-  }, {
-    title: 'Creative Developer',
-    company: 'RAPP',
-    website: 'https://rappfrance.com/',
-    from: new Date('2010-12-01'),
-    to: new Date('2013-05-01'),
-    location: 'Paris, France',
+      summary: `I'm in charge of developing and managing Flash and JS parts of web applications related to our core target (Facebook apps). I'm also the frontend consultant of the team ; I also write technical documentations and business recommandations.`,
 
-    summary: `I'm the frontend developer of the agency, using whatever stack needed to display content on the web. I also consult with with server-side developers, actively coding in php or C#. I'm also the preferred frontend consultant for creatives of the agency, project managers or business developers.`,
+      highlight: `Induction plan for javascript developers made for indian people : I had to sum up my own experience and organize it in lessons to teach people despite cultural and skill differences.`,
+    },
+    {
+      title: 'Creative Developer',
+      company: 'RAPP',
+      website: 'https://rappfrance.com/',
+      from: new Date('2010-12-01'),
+      to: new Date('2013-05-01'),
+      location: 'Paris, France',
 
-    highlight: `We were pushing boundaries of JS at that time, coding whatever designers wanted like 'multicolor web writable fonts' or 'video player with chapter and rich content overlayed'. A though time, but a very good time.`,
-  }, {
-    title: 'AS3 Developer',
-    company: 'Big Youth',
-    website: 'https://bigyouth.fr/',
-    from: new Date('2009-09-01'),
-    to: new Date('2010-12-01'),
-    location: 'Paris, France',
+      summary: `I'm the frontend developer of the agency, using whatever stack needed to display content on the web. I also consult with with server-side developers, actively coding in php or C#. I'm also the preferred frontend consultant for creatives of the agency, project managers or business developers.`,
 
-    summary: `I'm working on full-flash websites with multilingual, deeplinking and amfphp (server-side) web services. I'm in the curation group of the team and help by reviewing frameworks and technical solutions for our daily challenges related to flash.`,
+      highlight: `We were pushing boundaries of JS at that time, coding whatever designers wanted like 'multicolor web writable fonts' or 'video player with chapter and rich content overlayed'. A though time, but a very good time.`,
+    },
+    {
+      title: 'AS3 Developer',
+      company: 'Big Youth',
+      website: 'https://bigyouth.fr/',
+      from: new Date('2009-09-01'),
+      to: new Date('2010-12-01'),
+      location: 'Paris, France',
 
-    highlight: `[The Bic wall](https://thefwa.com/cases/the-bic-wall): the project is a huge realtime collaborative whiteboard. I had to do the R&D for the realtime server of the project. We had to code "from scratch udp/tcp servers" in many languages including C, java, python, nodejs and AS3 and test many existing solutions.`,
-  }, {
-    title: 'Junior UX Designer',
-    company: 'Axance',
-    website: 'https://axance.fr/',
-    from: new Date('2007-08-01'),
-    to: new Date('2009-09-01'),
-    location: 'Paris, France',
+      summary: `I'm working on full-flash websites with multilingual, deeplinking and amfphp (server-side) web services. I'm in the curation group of the team and help by reviewing frameworks and technical solutions for our daily challenges related to flash.`,
 
-    summary: `I'm working along with a project manager in user centric design refactorings. We design personas, usecases, sitemaps, wireframes, and user tests. I'm also developing prototypes for A/B user tests.`,
+      highlight: `[The Bic wall](https://thefwa.com/cases/the-bic-wall): the project is a huge realtime collaborative whiteboard. I had to do the R&D for the realtime server of the project. We had to code "from scratch udp/tcp servers" in many languages including C, java, python, nodejs and AS3 and test many existing solutions.`,
+    },
+    {
+      title: 'Junior UX Designer',
+      company: 'Axance',
+      website: 'https://axance.fr/',
+      from: new Date('2007-08-01'),
+      to: new Date('2009-09-01'),
+      location: 'Paris, France',
 
-    // highlight: `We had to design user-testing for NFC payment as it was just the beginning of it. In partnership with many telecom operators and banks, we helped them designing gestures and processes for the NFC contact-less payments. I had fun designing fake contact-less interactions with a flash-lite enabled nokia N90 phone and some computers.`,
-  }],
+      summary: `I'm working along with a project manager in user centric design refactorings. We design personas, usecases, sitemaps, wireframes, and user tests. I'm also developing prototypes for A/B user tests.`,
+
+      // highlight: `We had to design user-testing for NFC payment as it was just the beginning of it. In partnership with many telecom operators and banks, we helped them designing gestures and processes for the NFC contact-less payments. I had fun designing fake contact-less interactions with a flash-lite enabled nokia N90 phone and some computers.`,
+    },
+  ],
 
   skillsets: {
     human: {
-      availability: .625,
-      technicity: .875,
-      anticipation: .75,
+      availability: 0.625,
+      technicity: 0.875,
+      anticipation: 0.75,
       reactivity: 1,
     },
 
     languages: {
+      typescript: 1,
       javascript: 1,
-      htmlcss: .875,
+      htmlcss: 0.875,
       actionscript: 1,
-      php: .75,
-      cs: .625,
-      python: .5,
-      java: .625,
-      ruby: .375,
-      bash: .75,
+      php: 0.75,
+      cs: 0.625,
+      python: 0.5,
+      java: 0.625,
+      ruby: 0.375,
+      bash: 0.75,
     },
 
     machine: {
-      linux: .75,
-      vms: .875,
-      cloud: .75,
-      automation: .75,
-      adobe: .75,
-      sketch: .5,
-      figma: .75,
+      linux: 0.75,
+      docker: 0.5,
+      k8s: 0.5,
+      vms: 0.875,
+      cloud: 0.75,
+      automation: 0.75,
+      adobe: 0.75,
+      sketch: 0.5,
+      figma: 0.75,
     },
   },
 }
